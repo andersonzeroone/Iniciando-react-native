78cb4c2b3542b5d3a25807c532d8f61744dd5fb9
anderson <anderson.pablo02@gmail.com>
2019-06-23 18:52:03 -0300

    Adicionando State

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -4,8 +4,12 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/simples'
 import ParImpar from './componentes/ParImpar'
 import {Inverter, MegaSena} from './componentes/Milti'
+import Contador from './componentes/Contador'
 
 export default createDrawerNavigator({
+    Contador:{
+        screen : () => <Contador />
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={8} />,
         navigationOptions: { title: 'Mega Sena'}
